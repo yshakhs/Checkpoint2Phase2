eaf022181226cf6284feedb564be07f39e2629c2
Yara Alshakhs“git config --global user.email yshakhs@andrew.cmu.edu” <your@email.address>
2021-03-10 20:10:08 +0300

    all relationships and scopes are done

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,35 @@
 class Organization < ApplicationRecord
     has_many :students
     has_many :teams
+    has_many :student_teams, through: :students
+    has_many :student_teams, through: :teams
+
+
+    scope :active, -> { where(active: true) }
+    scope :inactive, -> { where(active: false) }
+    scope :alphabetical, -> { order('name') }
+
+    validates_presence_of :name
+    validates_presence_of :street_1
+    validates_presence_of :zip
+    validates_presence_of :short_name
+
+    validates_format_of :zip, with: /\A\d{5}\z/, message: "should be five digits long", allow_blank: true
+    validates_uniqueness_of :short_name, message:"short_name already in the records"
+
+    validates_inclusion_of :state, in: %w[AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI WY], message: "is not an option"
+
+
+    def make_active
+  self.active=true
+  self.save!
+end
+
+      
+def make_inactive
+    self.active=false
+    self.save!
+end
+
+
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,43 @@
 class Team < ApplicationRecord
   belongs_to :organization_id
-  has_and_belongs_to_many :teams
+  has_and_belongs_to_many :students
+
+  validates_presence_of :name
+  validates_presence_of :organization_id
+  validates_presence_of :division
+
+
+  scope :active, -> { where(active: true) }
+  scope :inactive, -> { where(active: false) }
+  scope :alphabetical, -> { order('name') }
+  scope :juniors, -> { where(division: Junior) }
+  scope :seniors, -> { where(division: Senior) }
+  
+  validates_inclusion_of :division, in: %w[Junior Senior], message: "is not an option"
+
+
+  def make_active
+    self.active=true
+    self.save!
+  end
+  
+        
+  def make_inactive
+      self.active=false
+      self.save!
+  end
+
+  scope :for_organization, -> (organization) { where("organization_id = ?", organization_id) }
+
+
+  private
+  def organization_is_active
+    # get an array of all active owners in the PATS system
+    all_owner_ids = Organization.active.all.map{|o| o.id}
+    # add error unless the owner id of the pet is in the array of active owners
+    unless all_owner_ids.include?(self.owner_id)
+      errors.add(:owner, "is not an active owner in PATS")
+    end
+  end
+  
 end
