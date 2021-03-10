57b49d5c7807ffb3cdf37bd8d98b8d33464cc221
Yara Alshakhs“git config --global user.email yshakhs@andrew.cmu.edu” <your@email.address>
2021-03-10 20:53:26 +0300

    all relationships and scopes with more changes

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -17,7 +17,8 @@ class Organization < ApplicationRecord
     validates_format_of :zip, with: /\A\d{5}\z/, message: "should be five digits long", allow_blank: true
     validates_uniqueness_of :short_name, message:"short_name already in the records"
 
-    validates_inclusion_of :state, in: %w[AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI WY], message: "is not an option"
+   STATES_LIST = [['Alabama', 'AL'],['Alaska', 'AK'],['Arizona', 'AZ'],['Arkansas', 'AR'],['California', 'CA'],['Colorado', 'CO'],['Connectict', 'CT'],['Delaware', 'DE'],['District of Columbia ', 'DC'],['Florida', 'FL'],['Georgia', 'GA'],['Hawaii', 'HI'],['Idaho', 'ID'],['Illinois', 'IL'],['Indiana', 'IN'],['Iowa', 'IA'],['Kansas', 'KS'],['Kentucky', 'KY'],['Louisiana', 'LA'],['Maine', 'ME'],['Maryland', 'MD'],['Massachusetts', 'MA'],['Michigan', 'MI'],['Minnesota', 'MN'],['Mississippi', 'MS'],['Missouri', 'MO'],['Montana', 'MT'],['Nebraska', 'NE'],['Nevada', 'NV'],['New Hampshire', 'NH'],['New Jersey', 'NJ'],['New Mexico', 'NM'],['New York', 'NY'],['North Carolina','NC'],['North Dakota', 'ND'],['Ohio', 'OH'],['Oklahoma', 'OK'],['Oregon', 'OR'],['Pennsylvania', 'PA'],['Rhode Island', 'RI'],['South Carolina', 'SC'],['South Dakota', 'SD'],['Tennessee', 'TN'],['Texas', 'TX'],['Utah', 'UT'],['Vermont', 'VT'],['Virginia', 'VA'],['Washington', 'WA'],['West Virginia', 'WV'],['Wisconsin ', 'WI'],['Wyoming', 'WY']]
+    validates_inclusion_of :state, in: STATES_LIST.map {|key, value| value}, message: "is not an option", allow_blank: true
 
 
     def make_active
--- app/models/team.rb
+++ app/models/team.rb
@@ -27,16 +27,17 @@ class Team < ApplicationRecord
       self.save!
   end
 
-  scope :for_organization, -> (organization) { where("organization_id = ?", organization_id) }
+  scope :for_organization, -> (organization) { where("organization_id = ?", organization) }
 
+  validate :organization_is_active_in_system
 
   private
-  def organization_is_active
-    # get an array of all active owners in the PATS system
-    all_owner_ids = Organization.active.all.map{|o| o.id}
-    # add error unless the owner id of the pet is in the array of active owners
-    unless all_owner_ids.include?(self.owner_id)
-      errors.add(:owner, "is not an active owner in PATS")
+  def organization_is_active_in_system
+    # get an array of all active organizations in the system
+    all_org_ids = Organization.active.all.map{|o| o.id}
+    # add error unless the organization id of the team is in the array of active organizations
+    unless all_org_ids.include?(self.organization_id)
+      errors.add(:organization, "is not an active organization in the system")
     end
   end
   
