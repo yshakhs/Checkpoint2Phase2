4f58a24baa6801672ebbca902a1063ba1717b882
Yara Alshakhs“git config --global user.email yshakhs@andrew.cmu.edu” <your@email.address>
2021-03-11 13:30:01 +0300

    organization method added

--- test/factories/organizations.rb
+++ test/factories/organizations.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
     factory :organization do
-      name {"TAMU" }
-      street_1 {"EC street1" }
-      zip {"77449"}
-      short_name {"TAMU"}
+      name { "TAMU" }
+      street_1 { "EC street1" }
+      zip { "77449" }
+      short_name { "TAMU" }
       active { true }
     end
   end
